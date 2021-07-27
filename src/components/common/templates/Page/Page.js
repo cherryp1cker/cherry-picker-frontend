f81203087cf0a476fca3f469cc94deaa421d0d27
cadenzah <cadenzah93@gmail.com>
2021-07-27 23:13:13 +0900

    모바일 화면 대응하도록 전반적인 퍼블리싱
    - 스크롤/핀치줌 기능 제거
    - 가로/세로 길이 안정적으로 유지되도록 조정

---
+++ src/components/common/templates/Page/Page.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Page.scoped.scss';
+
+import NavigationBar from '@/components/common/NavigationBar';
+import TabBar from '@/components/common/TabBar';
+
+// Material-UI
+import { Paper } from '@material-ui/core';
+
+const Page = (props) => {
+    return (
+        <Paper
+            className="template-page"
+            square
+        >
+            <NavigationBar />
+            <div className="content-container">
+                {props.children}
+            </div>
+            <TabBar />
+        </Paper>
+    );
+};
+
+export default Page;
