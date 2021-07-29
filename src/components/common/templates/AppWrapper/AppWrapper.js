ede8993989cd7f6d687b246c772700a8b24bbcc7
cadenzah <cadenzah93@gmail.com>
2021-07-29 23:20:54 +0900

    컴포넌트 이름 변경 (Wrapper -> AppWrapper)

--- src/components/common/templates/AppWrapper/AppWrapper.js
+++ src/components/common/templates/AppWrapper/AppWrapper.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import './Wrapper.scoped.scss';
+import './AppWrapper.scoped.scss';
 
 // Material-UI
 import Container from '@material-ui/core/Container';
 
 // 스크린 내 화면을 반응형으로 정렬
 // PC 화면에서는 2개 Breakpoint, 모바일에서는 Full-screen
-const Wrapper = (props) => {
+const AppWrapper = (props) => {
     return (
         <Container
             // maxWidth="lg"
-            className="template-wrapper"
+            className="template-app-wrapper"
         >
             {props.children}
         </Container>
     );
 };
 
-export default Wrapper;
+export default AppWrapper;
