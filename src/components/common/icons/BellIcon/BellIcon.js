caceea8238d7680a0057f446ed151c98cdf32182
cadenzah <cadenzah93@gmail.com>
2021-08-01 12:46:28 +0900

    아이콘 에셋 해상도 향상

--- src/components/common/icons/BellIcon/BellIcon.js
+++ src/components/common/icons/BellIcon/BellIcon.js
@@ -9,25 +9,25 @@ export default function BellIcon(props) {
         <IconButton className="bell-icon">
             <SvgIcon
                 {...props}
-                version="1.1"
-                id="Layer_1"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                x="0px"
-                y="0px"
-                width="32px"
-                height="32px"
+                width="32"
+                height="32"
                 viewBox="0 0 32 32"
-                enableBackground="new 0 0 32 32"
-                xmlSpace="preserve"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
             >
-                <image
-                    id="image0"
-                    width="32"
-                    height="32"
-                    x="0"
-                    y="0"
-                    href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAdVBMVEUAAADPz//J2fnI2PnI2PrJ2PrH1/fJ1vnI2PnJ2fnH1/rG2PjH1/fF2vrH1/rI2frG1vjI2PjF2vTH1/fI2PnI2frJ2frF1PTK2vrP3//I2PrH1/nI2PrG1vjH1/nI1vrJ2fvI2PrH1/nG1vbH1/vI2Pr////BOrbmAAAAJXRSTlMAEFCv389gUN9/wHAgMGDvcHAwQLCfzzAwEO/gz7CgkM+/gHBAd30NbgAAAAFiS0dEJloImLUAAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQflBxsMLzZr2XiPAAAAy0lEQVQ4y72T2xaCIBBFIakUDbyU2j2t8/+/GC1WCRP0UqvzxGG2wwyMjP1VfJYIYJ4sIvGliVqlQSQDZF4wtlIGzEJxXT7XVYBQQD25BmgIIFC6tkJKE6T0Az/FGqTwHBvPt+h8oIfwPEBq2mL3GaA7XwP0RKM9Csc1OFDA70vhSIETlOdKCuQ4exfbUaB3y3p7iYcuzmu0CIyQwvBaDwhq6qwWobiuaVINbm8NLKwBo21QRgAziVfOzXTHfgw22sNHFpWS0PLGfqs77VEW+2bZ2xkAAAAldEVYdGRhdGU6Y3JlYXRlADIwMjEtMDctMjdUMTI6NDc6NTMrMDA6MDCuEcn0AAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIxLTA3LTI3VDEyOjQ3OjUzKzAwOjAw30xxSAAAAABJRU5ErkJggg=="
+                <path
+                    d="M7.02547 13C7.02381 11.814 7.25691 10.6394 7.71132 9.54395C8.16573 8.44847 8.83248 7.45376 9.67311 6.61716C10.5138 5.78056 11.5117 5.11861 12.6093 4.66948C13.707 4.22034 14.8827 3.9929 16.0686 4.00026C21.0173 4.03705 24.9757 8.15043 24.9757 13.113V14C24.9757 18.477 25.9124 21.0749 26.7373 22.4948C26.8262 22.6465 26.8735 22.819 26.8744 22.9948C26.8754 23.1706 26.8299 23.3436 26.7427 23.4963C26.6554 23.6489 26.5295 23.7759 26.3775 23.8643C26.2256 23.9528 26.053 23.9996 25.8771 24H6.12304C5.9472 23.9996 5.77458 23.9527 5.6226 23.8643C5.47062 23.7758 5.34466 23.6488 5.25743 23.4961C5.1702 23.3435 5.12478 23.1705 5.12575 22.9946C5.12672 22.8188 5.17405 22.6463 5.26296 22.4946C6.08837 21.0746 7.02546 18.4767 7.02546 14L7.02547 13Z"
+                    stroke="#C8D8FA"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />
+                <path
+                    d="M12 24V25C12 26.0609 12.4214 27.0783 13.1716 27.8284C13.9217 28.5786 14.9391 29 16 29C17.0609 29 18.0783 28.5786 18.8284 27.8284C19.5786 27.0783 20 26.0609 20 25V24"
+                    stroke="#C8D8FA"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 />
             </SvgIcon>
         </IconButton>
