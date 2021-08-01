df143675c02944aec483f7619bf264fa08d3d846
SEOJIN-LEE <55384156+SEOJIN-LEE@users.noreply.github.com>
2021-08-01 16:56:02 +0900

    SearchBarContainer 추가 후 자잘한 작성

---
+++ src/containers/SearchBarContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import SearchBar from '@/components/search/SearchBar';
+
+const SearchBarContainer = (props) => {
+    <div>
+        <SearchBar />
+    </div>
+}
--- src/containers/SearchResultContainer.js
+++ src/containers/SearchResultContainer.js
@@ -3,13 +3,11 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import ResultList from '@/components/search/ResultList';
-import SearchBar from '@/components/search/SearchBar';
 import Page from '@/components/common/templates/Page';
 
 const SearchResultContainer = (props) => {
     return (
         <Page>
-            <SearchBar />
             <ResultList />
         </Page>
     );
