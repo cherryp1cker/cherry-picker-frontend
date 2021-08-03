dbd414f5d303070b308c846b5749e94c490d1061
cadenzah <cadenzah93@gmail.com>
2021-08-03 22:41:34 +0900

    필터 내 영역 구분선, Collapse형 컨테이너, 버튼 추가
    - 버튼은 공용 컴포넌트 1 개발 항목으로 대체 예정

--- src/components/search/SearchFilterModal/SearchFilterModal.js
+++ src/components/search/SearchFilterModal/SearchFilterModal.js
@@ -10,10 +10,21 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import Radio from '@material-ui/core/Radio';
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Collapse from '@material-ui/core/Collapse';
+
+
 import Modal from '@/components/common/templates/Modal';
+
+const MENU_TYPE = ['양식', '한식', '일식', '중식', '퓨전', '기타'];
 
 const Page = (props) => {
     const { isOpen } = props;
+    const [open01, setOpen01] = React.useState(false);
+    const handleClick = () => {
+        setOpen01(!open01);
+      };
 
     return (
         <Modal
@@ -21,8 +32,11 @@ const Page = (props) => {
             isOpen={isOpen}
         >
             <MuiDialogContent>
-                <FormControl component="fieldset">
-                    <RadioGroup row onChange={(e) => { console.log(e.target.value); }}>
+                <FormControl>
+                    <RadioGroup
+                        row
+                        onChange={(e) => { console.log(e.target.value); }}
+                    >
                         <FormControlLabel
                             value="location"
                             control={<Radio />}
@@ -37,6 +51,28 @@ const Page = (props) => {
                         />
                     </RadioGroup>
                 </FormControl>
+                <div>
+                    FILTER
+                </div>
+                <div>
+                    {MENU_TYPE.map((type, index) => (
+                        <button key={index}>{type}</button>
+                    ))}
+                </div>
+                <ListItem button onClick={handleClick}>
+                    {/* <ListItemText primary="Inbox" /> */}
+                    {open01 ? <span>닫기</span> : <span>열기</span>}
+                </ListItem>
+                <Collapse in={open01} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <ListItem button>
+                            {/* <ListItemText primary="Starred" /> */}
+                            {MENU_TYPE.map((type, index) => (
+                                <button key={index}>{type}</button>
+                            ))}
+                        </ListItem>
+                    </List>
+                </Collapse>
             </MuiDialogContent>
         </Modal>
     );
