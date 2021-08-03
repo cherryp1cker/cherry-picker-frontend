745581899aac556421805fcfd2c8e9fc983c2115
SEOJIN-LEE <55384156+SEOJIN-LEE@users.noreply.github.com>
2021-08-04 04:32:59 +0900

    SearchBar, Item 수정

--- src/components/common/lists/ItemList/ItemList.js
+++ src/components/common/lists/ItemList/ItemList.js
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './ItemList.scoped.scss'
-
-import Item from '@/components/common/Item/Item';
 
 
 const ItemList = (props) => {
